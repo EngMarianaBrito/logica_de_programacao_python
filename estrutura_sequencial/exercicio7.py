3cc9e64a8d373195aa9e91483fec6fe5ec597548
EngMarianaBrito <marianabrito1791@gmail.com>
2021-08-14 20:36:50 -0300

    Organizando em pastas

---
+++ estrutura_sequencial/exercicio7.py
@@ -0,0 +1,7 @@
+lado = float(input("Informe o valor da lado : "))
+
+area = lado * lado
+
+area_dobro = area * 2
+
+print(f"O dobro do area é {area_dobro} ")
---
+++ estrutura_sequencial/exercicio8.py
@@ -0,0 +1,7 @@
+salario = float(input("Informe quanto voce ganha por hora trabalhada: "))
+
+hora_trabalhada = float(input("Quantas horas você trabalhou ? "))
+
+salario_total = salario * hora_trabalhada
+
+print(f"O seu salario final do mês é {salario_total}")
