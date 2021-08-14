0aba975a456319ba3e2457671e22e0e70708fdda
EngMarianaBrito <marianabrito1791@gmail.com>
2021-08-14 20:47:27 -0300

    Update

---
+++ estrutura_sequencial/exercicio1.py
@@ -0,0 +1,4 @@
+#Perguntas no site abaixo
+#https://wiki.python.org.br/EstruturaSequencial
+
+print("Hello Word ")
---
+++ estrutura_sequencial/exercicio9.py
@@ -0,0 +1,5 @@
+fahrenheit = float(input("Informe os graus em Fahrenheit: "))
+
+celsius = 5 * ((fahrenheit-32)/9)
+
+print(f"O valor convertido para celsius é {celsius}")
