848905be526012e80da80fdb362be267650ac5dc
EngMarianaBrito <marianabrito1791@gmail.com>
2021-08-18 00:36:40 -0300

    Novas pasta de exercicios

---
+++ estrutura_decisao/exercicio1.py
@@ -0,0 +1,9 @@
+#https://wiki.python.org.br/EstruturaDeDecisao
+
+number1 = float(input("Informe um numero real real: "))
+number2 = float(input("Informe um segundo numero real real: "))
+
+if(number1 >= number2):
+    print(f"O maior numero é {number1}")
+else:
+    print(f"O maior numero é {number2}")
---
+++ estrutura_decisao/exercicio2.py
@@ -0,0 +1,6 @@
+number1 = float(input("Informe um numero: "))
+
+if(number1 >= 0):
+    print(f"O maior numero é POSITIVO")
+else:
+    print(f"O maior numero é NEGATIVO")
