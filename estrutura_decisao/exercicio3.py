891f758f376a368d3d1e138ed20aa291bc4079c5
EngMarianaBrito <marianabrito1791@gmail.com>
2021-08-18 00:43:19 -0300

    Exercicio 3  e 4

---
+++ estrutura_decisao/exercicio3.py
@@ -0,0 +1,8 @@
+sexo = str(input('Digite F para Feminino ou M para Masculino: ').upper())
+
+if sexo == 'M':
+    print("Sexo Masculino")
+elif sexo == 'F':
+    print("Sexo Feminino.")
+else:
+    print("Inválido.")
