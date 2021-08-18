3358cb90332d69d9ad2d1dcfe35cccc2cbc55a34
EngMarianaBrito <marianabrito1791@gmail.com>
2021-08-18 17:41:12 -0300

    Iniciando repetiçoes

---
+++ estrutura_repeticao/exercicio1.py
@@ -0,0 +1,8 @@
+#https://wiki.python.org.br/EstruturaDeRepeticao
+number = float(input("Informe um numero entre 1 a 10: "))
+
+if (number < 0):
+    if (number > 10):
+        print("Invalido")
+else:
+    print("É valido")
