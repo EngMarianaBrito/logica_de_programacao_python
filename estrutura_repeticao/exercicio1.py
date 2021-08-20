94a81b1d0c0827bd2b9562ab757dae90c5c6b7e9
EngMarianaBrito <marianabrito1791@gmail.com>
2021-08-19 22:09:20 -0300

    Uso do operador ou/or

--- estrutura_repeticao/exercicio1.py
+++ estrutura_repeticao/exercicio1.py
@@ -1,8 +1,7 @@
 #https://wiki.python.org.br/EstruturaDeRepeticao
 number = float(input("Informe um numero entre 1 a 10: "))
 
-if (number < 0):
-    if (number > 10):
-        print("Invalido")
+if (number < 0 or number > 10):
+    print("Invalido")
 else:
     print("É valido")
